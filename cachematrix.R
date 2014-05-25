f4ef681167fddf3c7dd5973e2f7ab344d382a731
Carlos Alcala <alcala21@gmail.com>
2014-05-25 18:54:45 -0500

    Modified the comments in the functions.

--- cachematrix.R
+++ cachematrix.R
@@ -14,14 +14,28 @@
 ## c) Set the value of the inverse
 ## d) Get the value of the inverse
 makeCacheMatrix <- function(x = matrix()) {
+	
+	# Start an empty matrix to hold the inverse value
 	inverse <- NULL
+
+	# This function sets the value of a new matrix, 
+	# and clears the previous cached inverse matrix
 	set <- function(y) {
 		x <<- y
 		inverse <<- NULL
 	}
+
+	# This function gets you the value of the stored matrix
 	get <- function() x
-	setInverse <- function(locInverse) inverse <- locInverse
+
+	# This function sets the value of the inverse matrix in the cache	
+	setInverse <- function(externalInverse) inverse <- externalInverse
+
+	# This function gets the inverse matrix stored in the cache
 	getInverse <- function() inverse
+
+	# Return a list with functions to set and get the matrix
+	# and its inverse, respectively.
 	list(set = set, get = get, 
 		setInverse = setInverse, getInverse = getInverse)
 }
@@ -32,13 +46,25 @@ makeCacheMatrix <- function(x = matrix()) {
 ## cacheSolve returns the inverse of a 'matrix' if it has already been
 ## calculated; otherwise, it calculates the inverse.
 cacheSolve <- function(x) {
+
+	# Check if the inverse is stored in the cache.
+	# If so, return the cached inverse.
 	inverse <- x$getInverse()
 	if(!is.null(inverse)) {
 		message('getting cached data')
 		return(inverse)
 	}
+	
+	# In case the inverse is not cached, let's calculate the inverse.
+	# Get the cached matrix in x.
 	tempMatrix <- x$get()
+
+	# Calculate the inverse matrix
 	inverse <- solve(tempMatrix)
+
+	# Store inverse matrix in the cache so it is not calculated again.
 	x$setInverse(inverse)
+
+	#Return the calculated inverse matrix
 	inverse
 }
